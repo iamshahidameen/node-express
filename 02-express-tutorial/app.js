1505463d101a2ceaa5b4ecec69353a1b4882831d
Shahid Ameen <iamshahidameen@gmail.com>
2023-02-25 02:03:57 +0500

    created server with different page urls and also with different status codes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,21 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/html' });
-  res.write('<h1>Welcome to my Homepage</h1>');
+  const url = req.url;
+
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Welcome to my Homepage</h1>');
+  } else if (url === '/contact') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Welcome to my Contact Page</h1>');
+  } else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write('<h1>Welcome to my About Page</h1>');
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write('<h1>Page not found</h1>');
+  }
   res.end();
 });
 
