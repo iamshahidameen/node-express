26cc8ab8645730ac714a4d252e58f470ea674472
Shahid Ameen <iamshahidameen@gmail.com>
2023-02-25 03:35:16 +0500

    Sent Response.json like an api

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,11 @@
 const express = require('express');
 
-const path = require('path');
+const { products } = require('./data');
 
 const app = express();
 
-app.use(express.static('./public'));
-
-// Adding below index.html into static resources folder(public). The app still runs because the index.html browser will pick automatically
-/*app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
-});*/
-
-app.all('*', (req, res) => {
-  console.log(path);
-  res.status(404).send('<h1>Resource Not Found</h1>');
+app.get('/', (req, res) => {
+  res.json(products);
 });
 
 app.listen(5000, () => {
