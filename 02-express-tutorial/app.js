5f594b81b5819f31bc446445df104a75e5157565
Shahid Ameen <iamshahidameen@gmail.com>
2023-02-25 02:39:36 +0500

    Created Homepage and server resources associated /require for homepage to function properly

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,14 +2,26 @@ const http = require('http');
 
 const { readFileSync } = require('fs');
 
-const homepage = readFileSync('./homepage.html');
+const homepageContent = readFileSync('./navbar-app/index.html');
+const homepageStyles = readFileSync('./navbar-app/styles.css');
+const homepageLogo = readFileSync('./navbar-app/logo.svg');
+const homepageJs = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
   if (url === '/') {
     res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(homepage);
+    res.write(homepageContent);
+  } else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homepageStyles);
+  } else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' });
+    res.write(homepageLogo);
+  } else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(homepageJs);
   } else if (url === '/contact') {
     res.writeHead(200, { 'content-type': 'text/html' });
     res.write('<h1>Welcome to my Contact Page</h1>');
