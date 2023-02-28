b22bdb38a561d2963397254a184fdc7a216f1380
Shahid Ameen <iamshahidameen@gmail.com>
2023-03-01 02:05:59 +0500

    refactored looger functio into a separate file

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,12 @@
 const express = require('express');
 
 const app = express();
-
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const path = req.path;
-  const date = new Date().getFullYear();
-  console.log(method, url, path, date);
-  next();
-};
+const logger = require('./logger');
 
 app.get('/', logger, (req, res) => {
   res.send('Home Page');
 });
 
 app.listen(5000, () => {
-  console.log('first express app listening on port 5000...');
+  console.log('Server is listening on port 5000...');
 });
