1ba3c63fee3c8461707f61108e747336178ab866
Shahid Ameen <iamshahidameen@gmail.com>
2023-03-01 01:47:21 +0500

    Log URL, Date, and request method in console

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,36 +2,16 @@ const express = require('express');
 
 const app = express();
 
-const { products } = require('./data');
-
 app.get('/', (req, res) => {
-  console.log(req);
+  const method = req.method;
+  const url = req.url;
+  const path = req.path;
+  const date = new Date().getFullYear();
 
+  console.log(method, url, path, date);
   res.send('Home Page');
-});
-app.get('/api/products', (req, res) => {
-  res.send('Products');
-});
-app.get('/api/v1/query', (req, res) => {
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    // res.status(200).send('No products matched your search');
-    return res.status(200).json({ success: true, data: [] });
-  }
-  console.log('search => ', search);
-  console.log('sortedProducts => ', sortedProducts);
-  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000...');
+  console.log('first express app listening on port 5000...');
 });
