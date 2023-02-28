dae57d0877cf8cd616c0d8c52394c6163cdd83c1
Shahid Ameen <iamshahidameen@gmail.com>
2023-03-01 01:51:37 +0500

    Using Middleware we created logger as a separate func and add this function it on a app.get in second argument. Express will automatically pass re, res, and next values to this function. Next is very important.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
+const logger = (req, res, next) => {
   const method = req.method;
   const url = req.url;
   const path = req.path;
   const date = new Date().getFullYear();
-
   console.log(method, url, path, date);
+};
+
+app.get('/', logger, (req, res) => {
   res.send('Home Page');
 });
 
