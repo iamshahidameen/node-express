ad58b685ab45c42db54ee193a1734957e7ec6806
Shahid Ameen <iamshahidameen@gmail.com>
2023-03-01 21:05:58 +0500

    Multiple middleware functions calling into multiple routes

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -6,6 +6,15 @@ const logger = require('./logger');
 app.get('/', logger, (req, res) => {
   res.send('Home Page');
 });
+app.get('/about', logger, (req, res) => {
+  res.send(' About Page');
+});
+app.get('/url', logger, (req, res) => {
+  res.send('URL Page');
+});
+app.get('/contact', logger, (req, res) => {
+  res.send('Contact Page');
+});
 
 app.listen(5000, () => {
   console.log('Server is listening on port 5000...');
--- 02-express-tutorial/logger.js
+++ 02-express-tutorial/logger.js
@@ -3,6 +3,7 @@ const logger = (req, res, next) => {
   const url = req.url;
   const date = new Date().getFullYear();
   console.log(method, url, date);
+  next();
 };
 
 module.exports = logger;
