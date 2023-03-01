e92dc9eaee61787108b4a83256eac5cf4e79287a
Shahid Ameen <iamshahidameen@gmail.com>
2023-03-02 01:11:03 +0500

    POST Method tested with JS Fetch GET/Post method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,7 +7,24 @@ let { people } = require('./data');
 //Static Assets/Homepage
 app.use(express.static('./methods-public'));
 //parse form data
+
+//Parse HTML Form Data
 app.use(express.urlencoded({ extended: false }));
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  console.log(name);
+  if (!name) {
+    return res
+      .status(401)
+      .json({ success: false, msg: 'Please provide name value' });
+  } else {
+    res.status(201).json({ success: true, person: name });
+  }
+});
 app.post('/loginPage', (req, res) => {
   console.log(req.body);
   const { name } = req.body;
