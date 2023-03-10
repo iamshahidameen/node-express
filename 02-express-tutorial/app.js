99ae250895ecb936712b04dd46fc98f4e55e3e4c
Shahid Ameen <iamshahidameen@gmail.com>
2023-03-11 01:55:50 +0500

    Express router setup

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,62 +1,26 @@
 const express = require('express');
-
 const app = express();
 
-let { people } = require('./data');
+const people = require('./routes/people');
 
-//Static Assets/Homepage
+// static assets
 app.use(express.static('./methods-public'));
-//parse form data
-
-//parse form data from JS fetch
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
 app.use(express.json());
 
-//Parse HTML Form Data
-app.use(express.urlencoded({ extended: false }));
-app.get('/api/people', (req, res) => {
-  res.status(200).json({ success: true, data: people });
-});
+app.use('/api/people', people);
 
-app.post('/api/people', (req, res) => {
-  const { name } = req.body;
-  console.log(name);
-  if (!name) {
-    return res
-      .status(401)
-      .json({ success: false, msg: 'Please provide name value' });
-  } else {
-    return res.status(201).json({ success: true, person: name });
-    //for Postman
-    // res.status(201).json({ success: true, data: [...people, name] });
-  }
-});
-app.post('/loginPage', (req, res) => {
-  console.log(req.body);
+app.post('/login', (req, res) => {
   const { name } = req.body;
   if (name) {
-    return res.status(200).json(`Welcome ${name}`);
-  } else {
-    return res.status(401).json('Please provide credentials');
-  }
-});
-
-app.delete('/api/people/:id', (req, res) => {
-  console.log('first');
-  const person = people.find((person) => person.id === Number(req.params.id));
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `No person with id ${req.params.id}` });
+    return res.status(200).send(`Welcome ${name}`);
   }
 
-  const newPeople = people.filter((person) => {
-    return person.id !== Number(req.params.id);
-  });
-  console.log('newPeople', newPeople);
-  return res.status(200).json({ success: true, data: newPeople });
+  res.status(401).send('Please Provide Credentials');
 });
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000...');
+  console.log('Server is listening on port 5000....');
 });
